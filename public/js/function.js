fe7b76ee1a97a6c5df9cb08653de9404758c5778
itsmechristian <cjvillanueva122213@gmail.com>
2018-03-14 00:16:41 +0000

    initial commit

--- public/js/function.js
+++ public/js/function.js
@@ -1,17 +1,16 @@
-// var boxLength = $(".box").length;
-// $(document).ready(function() {
-//   if (boxLength > 6) {
-//     $(".home-row").css("height", "auto");
-//   } else {
-//     $(".home-row").css("height", "950px");
-//   }
-// });
-
-
+var boxLength = $(".box").length;
+$(document).ready(function() {
+  if (boxLength > 6) {
+    $(".home-row").css("height", "auto");
+  } else {
+    $(".home-row").css("height", "950px");
+  }
+});
 
 // Make sidebar and Page same height
 
 window.onload = function() {
+  // Subpages Sidebar Height
   const pageHeight = $('.page').height()
   if(pageHeight > 500) {
   $('.sidebar').css('height', pageHeight);
@@ -19,5 +18,27 @@ window.onload = function() {
   else{
   $('.sidebar').css('height', 'auto');
   }
+  // Article Container Height
+  const bPost = $('.b-post').height()
+  if(bPost > 700){
+    $('.b-post-container').css('height', bPost);
+  }
 }
 
+$('.options > small').click(function() {
+  $('.items').toggleClass('clicked')
+})
+
+$(document).ready(function() {
+  $('.delete-article').on('click', function() {
+    const id = $(this).data('id')
+    $.ajax({
+      type: 'DELETE',
+      url: '/user/article/delete/'+id,
+      success: function(response) {
+        alert('Deleting Article');
+        window.location.href = '/user'
+      }
+    })
+  })
+})
